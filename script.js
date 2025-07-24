37fc4f65347c7fdfbbf24cea0506ac036e9d000a
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-07-24 21:59:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 ﻿function back() {
     window.location.href = "https://miroshkin0207.github.io/miroshkin"
 }
+
+let string = ""
+function input(text) {
+    string += text.innerHTML
+    document.querySelector(".output").innerHTML = string
+}
